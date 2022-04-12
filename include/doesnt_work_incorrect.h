00c7327a6aacf4de98d367b7451fb8ce6e2da3ce
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-12 12:24:00 +0200

    this crashes

--- include/doesnt_work_incorrect.h
+++ include/doesnt_work_incorrect.h
@@ -6,13 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 11:43:41 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/12 12:01:23 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/12 12:23:34 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DOESNT_WORK_INCORRECT
 # define DOESNT_WORK_INCORRECT
 
-// # define TEST "this is a test" // this crashes
+# define TEST "this is a test" // this crashes
 
 #endif
