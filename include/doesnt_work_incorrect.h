6c12c19e07f2501c3d24a6744bc1dc0820200441
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-12 12:02:54 +0200

    Tests added

---
+++ include/doesnt_work_incorrect.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   doesnt_work_incorrect.h                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 11:43:41 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/04/12 12:01:23 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOESNT_WORK_INCORRECT
+# define DOESNT_WORK_INCORRECT
+
+// # define TEST "this is a test" // this crashes
+
+#endif
---
+++ include/works_incorrect_03.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   works_incorrect_03.h                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 11:43:11 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/04/12 11:48:13 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WORKS_INCORRECT
+# define WORKS_INCORRECT
+
+# define TEST "this is a test"
+# include <stddef.h>
+
+#endif
